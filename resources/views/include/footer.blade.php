fc10ff37dbb2a2779673b7d396657ee0e3d6b8eb
gita_ramadani <gitaramadhani2412@gmail.com>
2024-03-06 08:51:01 +0700

    siimpegg

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,13 @@
+
+
+            <footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p></p>
+                    </div>
+                    <div class="float-end">
+
+                    </div>
+                </div>
+            </footer>
+        </div>
