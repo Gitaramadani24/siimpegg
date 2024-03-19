7301b6391857aba18d95913301f1fbce5a2bd18e
DizqyHidayat <dhdm1807@gmail.com>
2024-03-19 09:01:21 +0700

    dizky

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -37,6 +37,14 @@
                     <i class="bi bi-stack"></i>
                     <span>Components</span>
                 </a>
+                <ul class="submenu ">
+                    <li class="submenu-item ">
+                        <a href="table-datatable.html">Datatable</a>
+                    </li>
+                    <li class="submenu-item ">
+                        <a href="table-datatable-jquery.html">Datatable (jQuery)</a>
+                    </li>
+                </ul>
 
             </li>
 
@@ -46,6 +54,14 @@
                     <i class="bi bi-collection-fill"></i>
                     <span>Extra Components</span>
                 </a>
+                <ul class="submenu ">
+                    <li class="submenu-item ">
+                        <a href="table-datatable.html">Datatable</a>
+                    </li>
+                    <li class="submenu-item ">
+                        <a href="table-datatable-jquery.html">Datatable (jQuery)</a>
+                    </li>
+                </ul>
 
             </li>
 
@@ -54,64 +70,6 @@
                 <a href="#" class='sidebar-link'>
                     <i class="bi bi-grid-1x2-fill"></i>
                     <span>Layouts</span>
-                </a>
-
-            </li>
-
-            <li class="sidebar-title">Forms &amp; Tables</li>
-
-            <li
-                class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                    <i class="bi bi-hexagon-fill"></i>
-                    <span>Form Elements</span>
-                </a>
-
-            </li>
-
-            <li
-                class="sidebar-item  ">
-                <a href="form-layout.html" class='sidebar-link'>
-                    <i class="bi bi-file-earmark-medical-fill"></i>
-                    <span>Form Layout</span>
-                </a>
-            </li>
-
-            <li
-                class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                    <i class="bi bi-journal-check"></i>
-                    <span>Form Validation</span>
-                </a>
-                <ul class="submenu ">
-                    <li class="submenu-item ">
-                        <a href="form-validation-parsley.html">Parsley</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li
-                class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                    <i class="bi bi-pen-fill"></i>
-                    <span>Form Editor</span>
-                </a>
-
-            </li>
-
-            <li
-                class="sidebar-item  ">
-                <a href="table.html" class='sidebar-link'>
-                    <i class="bi bi-grid-1x2-fill"></i>
-                    <span>Table</span>
-                </a>
-            </li>
-
-            <li
-                class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                    <i class="bi bi-file-earmark-spreadsheet-fill"></i>
-                    <span>Datatables</span>
                 </a>
                 <ul class="submenu ">
                     <li class="submenu-item ">
@@ -122,185 +80,6 @@
                     </li>
                 </ul>
             </li>
-
-            <li class="sidebar-title">Extra UI</li>
-
-            <li
-                class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                    <i class="bi bi-pentagon-fill"></i>
-                    <span>Widgets</span>
-                </a>
-                <ul class="submenu ">
-                    <li class="submenu-item ">
-                        <a href="ui-widgets-chatbox.html">Chatbox</a>
-                    </li>
-                    <li class="submenu-item ">
-                        <a href="ui-widgets-pricing.html">Pricing</a>
-                    </li>
-                    <li class="submenu-item ">
-                        <a href="ui-widgets-todolist.html">To-do List</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li
-                class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                    <i class="bi bi-egg-fill"></i>
-                    <span>Icons</span>
-                </a>
-                <ul class="submenu ">
-                    <li class="submenu-item ">
-                        <a href="ui-icons-bootstrap-icons.html">Bootstrap Icons </a>
-                    </li>
-                    <li class="submenu-item ">
-                        <a href="ui-icons-fontawesome.html">Fontawesome</a>
-                    </li>
-                    <li class="submenu-item ">
-                        <a href="ui-icons-dripicons.html">Dripicons</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li
-                class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                    <i class="bi bi-bar-chart-fill"></i>
-                    <span>Charts</span>
-                </a>
-                <ul class="submenu ">
-                    <li class="submenu-item ">
-                        <a href="ui-chart-chartjs.html">ChartJS</a>
-                    </li>
-                    <li class="submenu-item ">
-                        <a href="ui-chart-apexcharts.html">Apexcharts</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li
-                class="sidebar-item  ">
-                <a href="ui-file-uploader.html" class='sidebar-link'>
-                    <i class="bi bi-cloud-arrow-up-fill"></i>
-                    <span>File Uploader</span>
-                </a>
-            </li>
-
-            <li
-                class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                    <i class="bi bi-map-fill"></i>
-                    <span>Maps</span>
-                </a>
-                <ul class="submenu ">
-                    <li class="submenu-item ">
-                        <a href="ui-map-google-map.html">Google Map</a>
-                    </li>
-                    <li class="submenu-item ">
-                        <a href="ui-map-jsvectormap.html">JS Vector Map</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="sidebar-title">Pages</li>
-
-            <li
-                class="sidebar-item  ">
-                <a href="application-email.html" class='sidebar-link'>
-                    <i class="bi bi-envelope-fill"></i>
-                    <span>Email Application</span>
-                </a>
-            </li>
-
-            <li
-                class="sidebar-item  ">
-                <a href="application-chat.html" class='sidebar-link'>
-                    <i class="bi bi-chat-dots-fill"></i>
-                    <span>Chat Application</span>
-                </a>
-            </li>
-
-            <li
-                class="sidebar-item  ">
-                <a href="application-gallery.html" class='sidebar-link'>
-                    <i class="bi bi-image-fill"></i>
-                    <span>Photo Gallery</span>
-                </a>
-            </li>
-
-            <li
-                class="sidebar-item  ">
-                <a href="application-checkout.html" class='sidebar-link'>
-                    <i class="bi bi-basket-fill"></i>
-                    <span>Checkout Page</span>
-                </a>
-            </li>
-
-            <li
-                class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                    <i class="bi bi-person-badge-fill"></i>
-                    <span>Authentication</span>
-                </a>
-                <ul class="submenu ">
-                    <li class="submenu-item ">
-                        <a href="auth-login.html">Login</a>
-                    </li>
-                    <li class="submenu-item ">
-                        <a href="auth-register.html">Register</a>
-                    </li>
-                    <li class="submenu-item ">
-                        <a href="auth-forgot-password.html">Forgot Password</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li
-                class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                    <i class="bi bi-x-octagon-fill"></i>
-                    <span>Errors</span>
-                </a>
-                <ul class="submenu ">
-                    <li class="submenu-item ">
-                        <a href="error-403.html">403</a>
-                    </li>
-                    <li class="submenu-item ">
-                        <a href="error-404.html">404</a>
-                    </li>
-                    <li class="submenu-item ">
-                        <a href="error-500.html">500</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="sidebar-title">Raise Support</li>
-
-            <li
-                class="sidebar-item  ">
-                <a href="https://zuramai.github.io/mazer/docs" class='sidebar-link'>
-                    <i class="bi bi-life-preserver"></i>
-                    <span>Documentation</span>
-                </a>
-            </li>
-
-            <li
-                class="sidebar-item  ">
-                <a href="https://github.com/zuramai/mazer/blob/main/CONTRIBUTING.md" class='sidebar-link'>
-                    <i class="bi bi-puzzle"></i>
-                    <span>Contribute</span>
-                </a>
-            </li>
-
-            <li
-                class="sidebar-item  ">
-                <a href="https://github.com/zuramai/mazer#donation" class='sidebar-link'>
-                    <i class="bi bi-cash"></i>
-                    <span>Donate</span>
-                </a>
-            </li>
-
         </ul>
     </div>
 </div>
